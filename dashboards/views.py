57bd454bdfce9172c3746c526775d6f49abe0fb0
AsarehSKP <promiseasareh@gmail.com>
2025-10-02 14:03:27 +0330

    chang dashboard user

--- dashboards/views.py
+++ dashboards/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def user_dashboard(request):
-    return render(request, 'dashboards/user_dash_index.html')
+    return render(request, 'dashboards/dashboard.html')
 
 
 
-def user_profile(request):
-    return render(request, 'dashboards/user_profile.html')
+# def user_profile(request):
+#     return render(request, 'dashboards/user_profile.html')
