86a68913a4385e55a7a65a786655b9f183c6c426
AsarehSKP <promiseasareh@gmail.com>
2025-10-02 14:03:27 +0330

    chang dashboard user

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = 'dashboards'
 urlpatterns = [
     path('dashboard/', user_dashboard, name='dashboard'),
-    path('user_profile/', user_profile, name='user_profile'),
+    # path('user_profile/', user_profile, name='user_profile'),
 
 ]
